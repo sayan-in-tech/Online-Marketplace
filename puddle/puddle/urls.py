2433fd60ad66d8ef99c0d13d0121fbb5596d2d89
Sayan Ghosh <sayan18012004@gmail.com>
2025-05-17 23:18:24 +0530

    first commit

---
+++ puddle/puddle/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from core.views import index, contact
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('admin/', admin.site.urls),
+    path('contact/', contact, name='contact'),
+]
