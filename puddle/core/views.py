b9007d0df9d9116c1bdeb036cb352c7c8fd2a7e2
Sayan Ghosh <sayan18012004@gmail.com>
2025-05-18 13:38:37 +0000

    May 18, 2025, 7:08 PM

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from item.models import Item, Category
 
 def index(request):
-    items = Item.objects.all()
+    items = Item.objects.filter(is_sold=False)
     return render(request, 'core/index.html')
 
 def contact(request):
